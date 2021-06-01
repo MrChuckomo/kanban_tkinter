9f86113301985a188eed25e780bc48cb907ecf13
MrChuckomo <alexander.kettler91@gmail.com>
2021-06-01 21:04:56 +0200

    Restructure project

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ __Version__       = "v1.0.0"
 __Creation_Date__ = "15-Jun-2017"
 """
 
-from view import board as BoardGui
-from model import kanban_db as Db
+from tkanban.view import board as BoardGui
+from tkanban.model import kanban_db as Db
 
 Db.InitDb()
 
