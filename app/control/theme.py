e3672a7b649a4ab6a8e8a3ae2ebed9643be366a6
MrChuckomo <alexander.kettler91@gmail.com>
2021-06-01 20:53:31 +0200

    Code cleanup theme.py

--- app/control/theme.py
+++ app/control/theme.py
@@ -1,50 +1,35 @@
 """
-__File__          = "theme.py"
-__Description__   = ""
-__Author__        = "MrChuckomo"
-__Version__       = "v1.0.0"
-__Creation_Date__ = "25-Jun-2017"
+File          : theme.py
+Description   : 
+
+Author        : MrChuckomo
+Version       : v1.0.0
+Creation Date : 25-Jun-2017
 """
 
-
-# / ----------------------------------------------------------------------------
-
-
-s_Light = "theme/macos_light.csv"
-s_Dark  = "theme/macos_dark.csv"
+s_Light = 'theme/macos_light.csv'
+s_Dark  = 'theme/macos_dark.csv'
 
 
-# / ----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 
-
-"""@package docstring
-    Function: GetTheme
-
+def get_theme(theme=s_Light) -> dict:
+    """
     Get one of the defined themes as a python dictionary.
     The dictionary gives you easy access to the values by using keywords.
 
-    Parameter:
+    Args:
+    - theme (str) - file path
 
-        _Theme = file path
-
-    Return:
-
-        Return a theme file as dictionary array.
-"""
-
-
-def GetTheme(_Theme=s_Light):
-
-    File = open(_Theme, "r")
-
-    FileContent = str(File.read()).split("\n")
+    Return (dict) a theme file as dictionary.
+    """
+    File = open(theme, 'r')
+    FileContent = str(File.read()).split('\n')
     ThemeDictionary = {}
 
     for Line in FileContent:
-
-        if Line != "":
-
-            SplitLine = str(Line).split("=")
+        if Line != '':
+            SplitLine = str(Line).split('=')
 
             Key   = SplitLine[0].strip()
             Value = SplitLine[1].strip()
@@ -54,5 +39,3 @@ def GetTheme(_Theme=s_Light):
     File.close()
 
     return ThemeDictionary
-
-# / ----------------------------------------------------------------------------
